0f6054aa44fa7ecbf51d5a676dd976ba7feefd97
chea-young <study.lchy@gmail.com>
2020-06-05 23:20:19 +0900

    Initial commit

---
+++ app/src/main/java/com/example/xproject/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.xproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var retrofit = Retrofit.Builder()
+                .baseUrl("http://10.10.1.67:8000")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        // baseUrl은 내 local 주소
+        var loginService: LoginService = retrofit.create(LoginService::class.java)
+
+        button.setOnClickListener {
+            var textId = editTextTextPersonName.text.toString()
+            var textPw = editTextTextPassword.text.toString()
+
+            loginService.requestLogin(textId, textPw).enqueue(object : Callback<Login> {
+                override fun onFailure(call: Call<Login>, t: Throwable) {
+                    Log.e("DEBUG", t.message)
+                    var dialog = AlertDialog.Builder(this@MainActivity)
+                    dialog.setTitle("실패")
+                    dialog.setMessage("통신에 실패했습니다..")
+                    dialog.show()
+                }
+                override fun onResponse(call: Call<Login>, response: Response<Login>) {
+                    var login = response.body()
+                    Log.d("LOGIN","msg : "+login?.msg)
+                    Log.d("LOGIN","code : "+login?.code)
+                    var dialog = AlertDialog.Builder(this@MainActivity)
+                    dialog.setTitle("알람!")
+                    dialog.setMessage("id = "+textId +" pw = "+textPw)
+                    dialog.setMessage("code = " + login?.code + " msg = " + login?.msg)
+                    dialog.show()
+                }
+            })
+        }
+    }
+}
